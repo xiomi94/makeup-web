3bcb833fc54bf824ba57b24e3b7403c4845866ee
xiomi94 <xiomarajimvel94@gmail.com>
2024-10-20 20:48:12 +0100

    add comment to explain how showHideSearchBar.js works

--- scripts/showHideSearchBar.js
+++ scripts/showHideSearchBar.js
@@ -1,3 +1,5 @@
+//This script works for doing a reactive search bar with click search icon
+
 function clickedSearchButton() {
     const searchInput = document.getElementById("search-input")
     if (searchInput.classList.contains("hidden-search-input")) {
